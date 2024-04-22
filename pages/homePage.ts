15986bc8babd5d19cbd83d1b05bc11a40248dc06
Hazim Okanovic <hazimokanovic@live.com>
2024-04-22 21:20:35 +0200

    Created small script for checking sold out products

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -2,8 +2,10 @@ import { Page, Locator} from "@playwright/test";
 
 export class HomePage{
     readonly page: Page;
+    readonly cartButton: Locator;
 
     constructor(page: Page){
         this.page = page;
+        this.cartButton = page.locator("//div[@class = 'thb-secondary-item-icon']");
     }
 }
---
+++ pages/mothersDayPage.ts
@@ -0,0 +1,19 @@
+import { Page, Locator} from "@playwright/test";
+
+export class MothersDayPage{
+    readonly page: Page;
+    readonly products: Locator
+    readonly quickAddToCartButtons: Locator
+    readonly productTitles: Locator
+    readonly soldOutMark: Locator
+    readonly soldOutProductName: Locator
+
+    constructor(page: Page){
+        this.page = page;
+        this.products = page.locator("//div[@class='featured__collection--inner display-type--grid layout__type--grid hide-mobile']//figure//a");
+        this.productTitles = page.locator("//div[@class='featured__collection--inner display-type--grid layout__type--grid hide-mobile']//figure//following-sibling::div[@class='product-card-info']//a")
+        this.quickAddToCartButtons = page.locator("//div[@class='featured__collection--inner display-type--grid layout__type--grid hide-mobile']//figure//ancestor::button")
+        this.soldOutMark = page.locator("//div[@class='featured__collection--inner display-type--grid layout__type--grid hide-mobile']//figure//span[@class = 'badge out-of-stock top left']")
+        this.soldOutProductName = page.locator("//div[@class='featured__collection--inner display-type--grid layout__type--grid hide-mobile']//figure//span[@class = 'badge out-of-stock top left']//parent::figure//following-sibling::div//a")
+    }
+}
